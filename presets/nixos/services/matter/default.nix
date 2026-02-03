b54ef0987700ee3af4dc3ce9a87b88e0ebb67d7f
Joy <20245490+Naterjack@users.noreply.github.com>
2026-02-03 19:15:00 +0000

    matter-server: enable wifi for matter

--- presets/nixos/services/matter/default.nix
+++ presets/nixos/services/matter/default.nix
@@ -3,8 +3,15 @@
 }:
 
 {
+  networking.wireless = {
+    enable = true;
+    userControlled = true;
+  };
   services.matter-server = {
     enable = true;
     openFirewall = true;
+    extraArgs = [
+      "--primary-interface wlan0"
+    ];
   };
 }
